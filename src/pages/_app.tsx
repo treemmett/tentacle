2cb5c36e8a519cfb5497f8ddfb7d402f30e7e7dc
Tregan <tregan@tregan.me>
2023-05-02 15:20:27 +0200

    feat: add mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
+import { Notifications } from '@mantine/notifications';
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { FC, useCallback, useEffect, useState } from 'react';
+import { emotionCache } from '@/utils/emotion';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useState(preferredColorScheme);
+
+  const toggleColorScheme = useCallback(
+    (value: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark')),
+    [colorScheme]
+  );
+
+  useEffect(() => {
+    setColorScheme(preferredColorScheme);
+  }, [preferredColorScheme]);
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        emotionCache={emotionCache}
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Notifications position="bottom-center" />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+};
+
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyles, createStylesServer } from '@mantine/next';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import { emotionCache } from '@/utils/emotion';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const stylesServer = createStylesServer(emotionCache);
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles html={initialProps.html} key="styles" server={stylesServer} />,
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
