7d672900ab236a7c90c3cd084ee0770e04a0b061
Tregan <tregan@tregan.me>
2023-05-09 23:39:00 +0200

    fix(vercel): prompt for installation

--- src/components/VercelIntegrationForm.tsx
+++ src/components/VercelIntegrationForm.tsx
@@ -1,13 +1,32 @@
-import { Center, Checkbox, Loader, Select, Text } from '@mantine/core';
+import { Button, Center, Checkbox, Loader, Select, Text } from '@mantine/core';
 import { UseFormReturnType } from '@mantine/form';
+import { IconBrandVercel } from '@tabler/icons-react';
 import { FC } from 'react';
 import type { TriggerFormValues } from './TriggerForm';
 import { useVercel } from '@/lib/vercel';
+import { IntegrationNotFoundError } from '@/utils/errors';
 
 export const VercelIntegrationForm: FC<{ form: UseFormReturnType<TriggerFormValues> }> = ({
   form,
 }) => {
   const { error, isLoading, projects } = useVercel();
+
+  if (error instanceof IntegrationNotFoundError) {
+    return (
+      <Button
+        component="a"
+        href="https://vercel.com/integrations/tentacle/new"
+        rel="noreferrer noopener"
+        target="_blank"
+        type="button"
+        variant="outline"
+        w="100%"
+      >
+        <IconBrandVercel stroke={2} />
+        <Text ml="sm">Login with Vercel</Text>
+      </Button>
+    );
+  }
 
   if (isLoading) {
     return (
--- src/lib/vercel.ts
+++ src/lib/vercel.ts
@@ -3,11 +3,16 @@ import { useUser } from './user';
 import type { GetVercelProjects } from '@/entities/VercelIntegration';
 import { api } from '@/utils/apiClient';
 import { VERCEL } from '@/utils/cacheKeys';
+import { APIError } from '@/utils/errors';
 
 export function useVercel() {
   const { user } = useUser();
 
-  const { data, error, isLoading } = useSWR(
+  const { data, error, isLoading } = useSWR<
+    GetVercelProjects,
+    APIError,
+    () => [string, typeof VERCEL] | null
+  >(
     () => (user ? [user.id, VERCEL] : null),
     () => api<GetVercelProjects>('GET', '/vercel/projects')
   );
